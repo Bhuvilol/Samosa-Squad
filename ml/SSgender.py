708d7384bb505d87b5f8e3291f93b0f264dd4ed3
Bhabesh <bhabeshcse@gmail.com>
2024-08-31 12:34:25 +0530

    emptied

--- ml/SSgender.py
+++
@@ -1,8 +0,0 @@
-from tensorflow.keras.preprocessing.image import img_to_array
-from tensorflow.keras.models import load_model
-import numpy as np
-import cv2
-import os
-import cvlib as cv
-
-model = load_model('gender_detection.keras')
